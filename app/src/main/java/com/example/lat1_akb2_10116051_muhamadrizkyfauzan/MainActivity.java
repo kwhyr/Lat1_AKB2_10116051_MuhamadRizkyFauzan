4c0c03f724f69b76dcd36d68f391af70b36b2151
kwhyr <rizkyc993@gmail.com>
2019-04-05 15:05:17 +0700

    NIM : 10116051 Nama : Muhamad Rizky Fauzan

---
+++ app/src/main/java/com/example/lat1_akb2_10116051_muhamadrizkyfauzan/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lat1_akb2_10116051_muhamadrizkyfauzan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class ));
+            }
+        });
+    }
+}
